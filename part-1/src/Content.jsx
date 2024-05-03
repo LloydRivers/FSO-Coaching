6b2cc1c4ffd5b6e18088d620765594412faf216a
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2024-05-03 13:56:18 +0100

    1.2: Course Information, step 2

--- part-1/src/Content.jsx
+++ part-1/src/Content.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Part from "./Part";
 
 const Content = ({
   part1,
@@ -10,15 +11,9 @@ const Content = ({
 }) => {
   return (
     <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
     </>
   );
 };
