116e15c0a1701bc04be2590d1d0df27cf482d28a
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2024-05-03 14:15:59 +0100

    1.5: Course Information step 5

--- part-1/src/App.jsx
+++ part-1/src/App.jsx
@@ -2,28 +2,30 @@ import Header from "./Header";
 import Content from "./Content";
 import Total from "./Total";
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
-  const total = parts.reduce((acc, cur) => acc + cur.exercises, 0);
+  const total = course.parts.reduce((acc, part) => acc + part.exercises, 0);
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
       <Total total={total} />
     </div>
   );
