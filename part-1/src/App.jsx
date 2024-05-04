e9904433ffcffd477a2b463663e061e9d7352313
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2024-05-04 19:21:13 +0100

    Moved completed exercises to courseinfo folder

--- part-1/src/App.jsx
+++ part-1/src/App.jsx
@@ -1,34 +1,7 @@
-import Header from "./Header";
-import Content from "./Content";
-import Total from "./Total";
+import React from "react";
+
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
-
-  const total = course.parts.reduce((acc, part) => acc + part.exercises, 0);
-
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total total={total} />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
