fb8ae489de48b160c4799e4d797028f276abe43d
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2024-05-06 20:07:12 +0100

    anecdotes step 1 boiler plate code

--- part-1/src/App.jsx
+++ part-1/src/App.jsx
@@ -1,80 +1,20 @@
 import { useState } from "react";
-import Statistics from "./Statistics";
-import Button from "./Button";
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const [totalFeedback, setTotalFeedback] = useState(0);
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
 
-  const handleGoodClick = () => {
-    setGood(good + 1);
-    setTotalFeedback(totalFeedback + 1);
-  };
+  const [selected, setSelected] = useState(0);
 
-  const handleNeutralClick = () => {
-    setNeutral(neutral + 1);
-    setTotalFeedback(totalFeedback + 1);
-  };
-
-  const handleBadClick = () => {
-    setBad(bad + 1);
-    setTotalFeedback(totalFeedback + 1);
-  };
-
-  return (
-    <>
-      {totalFeedback > 0 ? (
-        <>
-          <h1>give feedback</h1>
-          <Button handleClick={handleGoodClick} text="good" testId="good" />
-          <Button
-            handleClick={handleNeutralClick}
-            text="neutral"
-            testId="neutral"
-          />
-          <Button handleClick={handleBadClick} text="bad" testId="bad" />
-
-          <br />
-
-          <h1>statistics</h1>
-          <Statistics
-            testId="statistics"
-            good={good}
-            bad={bad}
-            neural={neutral}
-            totalFeedback={totalFeedback}
-          />
-        </>
-      ) : (
-        <>
-          <h1 data-testid="heading">give feedback</h1>
-          <Button
-            handleClick={handleGoodClick}
-            text="good"
-            testId="no-feedback-good"
-          />
-          <Button
-            handleClick={handleNeutralClick}
-            text="neutral"
-            testId="neutral"
-          />
-          <Button
-            handleClick={handleBadClick}
-            text="bad"
-            testId="no-feedback-bad"
-          />
-
-          <br />
-
-          <h2 data-testid="statistics-heading">statistics</h2>
-          <p>no feedback given</p>
-        </>
-      )}
-    </>
-  );
+  return <div>{anecdotes[selected]}</div>;
 };
 
 export default App;
