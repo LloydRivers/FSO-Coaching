fea5a8a8211ef7232daa9fa671da40a434aa51ef
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2024-05-07 15:09:12 +0100

    dummy data

--- part-2/src/App.tsx
+++ part-2/src/App.tsx
@@ -2,8 +2,12 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
+
   const [newPerson, setNewPerson] = useState({
     name: "",
     number: "",
@@ -18,6 +22,7 @@ const App = () => {
     const personObject = {
       name: newPerson.name,
       number: newPerson.number,
+      id: persons.length + 1,
     };
 
     setPersons(persons.concat(personObject));
