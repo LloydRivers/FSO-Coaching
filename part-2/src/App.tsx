38e9819725bf78e82a528e79d0368921a6dee791
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2024-05-07 14:51:15 +0100

    2.6: The Phonebook Step 1

--- part-2/src/App.tsx
+++ part-2/src/App.tsx
@@ -4,19 +4,38 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const addPerson: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+
+  const handlePersonChange: React.ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    setNewName(event.target.value);
+  };
+
   return (
     <div>
+      <div>debug: {newName}</div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handlePersonChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => (
+        <div key={person.name}>{person.name}</div>
+      ))}
     </div>
   );
 };
