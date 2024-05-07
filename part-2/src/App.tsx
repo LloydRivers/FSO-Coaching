beac22f956c5f8e3ea210e3796baa7487cb8be2b
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2024-05-07 12:43:01 +0100

    2.5: Separate module step10

--- part-2/src/App.tsx
+++ part-2/src/App.tsx
@@ -1,63 +1,7 @@
 import Course from "./Course";
-
-type CoursePart = {
-  name: string;
-  exercises: number;
-  id: number;
-};
-
-type CourseData = {
-  name: string;
-  id: number;
-  parts: CoursePart[];
-};
+import { courses } from "./data";
 
 const App = () => {
-  const courses: CourseData[] = [
-    {
-      name: "Half Stack application development",
-      id: 1,
-      parts: [
-        {
-          name: "Fundamentals of React",
-          exercises: 10,
-          id: 1,
-        },
-        {
-          name: "Using props to pass data",
-          exercises: 7,
-          id: 2,
-        },
-        {
-          name: "State of a component",
-          exercises: 14,
-          id: 3,
-        },
-        {
-          name: "Redux",
-          exercises: 11,
-          id: 4,
-        },
-      ],
-    },
-    {
-      name: "Node.js",
-      id: 2,
-      parts: [
-        {
-          name: "Routing",
-          exercises: 3,
-          id: 1,
-        },
-        {
-          name: "Middlewares",
-          exercises: 7,
-          id: 2,
-        },
-      ],
-    },
-  ];
-
   return <Course courses={courses} />;
 };
 
