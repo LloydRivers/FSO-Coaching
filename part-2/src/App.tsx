dfd87c666b0191f0566a6e2b6fe036441c6c5014
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2024-05-09 09:52:36 +0100

    2.20*: Data for countries, step 3

--- part-2/src/App.tsx
+++ part-2/src/App.tsx
@@ -1,14 +1,29 @@
 import axios from "axios";
 import { Root } from "./types";
 import { useState, useEffect } from "react";
+import { IWeatherData } from "./weatherTypes";
+
 const App = () => {
   const ALL_COUNTRIES = "https://studies.cs.helsinki.fi/restcountries/api/all";
-  const GET_BY_NAME = "https://studies.cs.helsinki.fi/restcountries/api/name";
+  const API_KEY = import.meta.env.VITE_API_KEY;
 
   const [countries, setCountries] = useState<Root[]>([]);
   const [filteredCountries, setFilteredCountries] = useState<Root[]>([]);
   const [filter, setFilter] = useState<string>("");
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const [weatherData, setWeatherData] = useState<IWeatherData | null>(null);
+
+  const fetchWeather = async (lat = 0, lon = 0) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+      const response = await axios.get(url);
+      const data = response.data;
+      setWeatherData(data);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  };
 
   const filterData = (data: Root[], filter: string) => {
     return data.filter((country) =>
@@ -40,9 +55,16 @@ const App = () => {
       console.log(error);
     }
   };
+
   useEffect(() => {
     fetchData();
-  }, []);
+
+    if (filteredCountries.length === 1) {
+      const lat = filteredCountries[0].latlng[0];
+      const lon = filteredCountries[0].latlng[1];
+      fetchWeather(lat, lon);
+    }
+  }, [weatherData]);
 
   return (
     <div>
@@ -67,6 +89,19 @@ const App = () => {
                 alt={filteredCountries[0].name.common}
                 width="100"
               />
+              <h2>Weather in {filteredCountries[0].capital[0]}</h2>
+
+              {weatherData ? (
+                <div>
+                  <h2>
+                    temperature {(weatherData.main.temp - 273.15).toFixed(1)}{" "}
+                    celcius{" "}
+                  </h2>
+                  <h2>wind {weatherData.wind.speed}m/s</h2>
+                </div>
+              ) : (
+                <p>Loading weather data...</p>
+              )}
             </div>
           ) : filteredCountries.length > 0 ? (
             filteredCountries.map((country) => (
