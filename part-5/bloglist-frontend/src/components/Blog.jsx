9c21fe29b576768553cfba1273356a9f07bc3132
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2024-06-19 20:15:53 +0100

    5.8

--- part-5/bloglist-frontend/src/components/Blog.jsx
+++ part-5/bloglist-frontend/src/components/Blog.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import blogService from "../services/blogs";
 
 const Blog = ({ blog, handleLike }) => {
   const [expanded, setExpanded] = useState(false);
